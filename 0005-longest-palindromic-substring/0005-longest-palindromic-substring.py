7d34b8746962cb0aef033596033ab663c98364a4
Christy <choiyoungeun@gmail.com>
2025-03-14 21:58:54 +0000

    Time: 311 ms (58.72%), Space: 18 MB (29.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,11 +4,10 @@ class Solution:
         res = ""
 
         for i in range(len(s)):
-
             # odd
             l, r = i, i
             while l>=0 and r < len(s) and s[l] == s[r]:
-                if r - l + 1 > resLen:
+                if (r - l + 1) > resLen:
                     resLen = r - l + 1
                     res = s[l:r+1]
                 l -= 1
@@ -16,11 +15,10 @@ class Solution:
 
             # even
             l, r = i, i+1
-            while l>0 and r< len(s) and s[l] == s[r]:
-                if r - l + 1 > resLen:
+            while l>=0 and r< len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
                     resLen = r - l + 1
                     res = s[l:r+1]
                 l -= 1
                 r += 1
-
         return res    
